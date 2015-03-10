ebbd8f69e3f1ea6a6d48092b2d3b3f023c9d09ae
Darshan Patil <darshanpatil123@yahoo.com>
2015-03-10 03:17:40 -0700

    Extracted FB Likes from user, started work on sending back to server

--- smartvocab/views.py
+++ smartvocab/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def index(request):
+    if(request.method == 'GET'):
+        context = {'message': 'Hello World!'}
+        return render(request, 'smartvocab/test.html', context)
+    else:
+        print request
+
 # Create your views here.
